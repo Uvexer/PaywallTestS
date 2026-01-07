bbe344daea80a4ce3ee0aec33cc76433a8bfbae9
Bogdan <bogdan@adforcegroup.com>
2026-01-07 11:40:42 +0300

    Initial Commit

---
+++ PaywallTestS/PaywallTestSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaywallTestSApp.swift
+//  PaywallTestS
+//
+//  Created by Bogdan Chupakhin on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PaywallTestSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
